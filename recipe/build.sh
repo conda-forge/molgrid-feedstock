64ab2231010e4b8ae8924b77ab6dea43fdf094f0
Hadrien Mary <hadrien.mary@gmail.com>
2025-01-28 09:43:33 -0500

    fix compilation error and enable linux_ppc64le

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,6 @@ set -ex
 NUMPY_INCLUDE_DIR=$(python -c "import numpy; print(numpy.get_include())")
 
 if [[ ${cuda_compiler_version} != "None" ]]; then
-  export USE_CUDA=1
   export NCCL_ROOT_DIR=$PREFIX
   export NCCL_INCLUDE_DIR=$PREFIX/include
   export USE_SYSTEM_NCCL=1
@@ -13,8 +12,13 @@ if [[ ${cuda_compiler_version} != "None" ]]; then
   export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
   export CUDNN_INCLUDE_DIR=$PREFIX/include
 else
-  export USE_CUDA=0
+  echo "molgrid does not support CPU build"
+  exit 1
 fi
+
+# NOTE: fix the error:
+# `error: 'virtual std::pair<int, float> libmolgrid::CallbackIndexTyper::get_atom_type_index(OpenBabel::OBAtom*) const' was hidden [-Werror=overloaded-virtual=]`
+export CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -Wno-error=overloaded-virtual"
 
 mkdir -p build/
 cd build/
@@ -25,6 +29,7 @@ cmake ${CMAKE_ARGS} .. \
   -DOPENBABEL3_INCLUDE_DIR=$PREFIX/include/openbabel3/ \
   -DOPENBABEL3_LIBRARIES=$PREFIX/lib/libopenbabel.so \
   -DPYTHON_NUMPY_INCLUDE_DIR=$NUMPY_INCLUDE_DIR \
+  -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} \
   -DBUILD_STATIC=0 \
   -DBUILD_SHARED=1
 
